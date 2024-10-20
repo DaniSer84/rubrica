f3339e8b5732a4f26f01388866aaa5d3153451e4
DaniSer84 <serenelli84@gmail.com>
2024-10-20 14:44:50 +0200

    createContactTable implemented

--- DatabaseAbstraction/src/Helper.php
+++ DatabaseAbstraction/src/Helper.php
@@ -3,9 +3,8 @@
 namespace Daniser\Rubrica;
 class Helper {
 
-    public static function createContactListItem( array $item): string
-    {
-    
+    public static function createNameSurnameListItem( array $item) {
+
         return "<li class='list-group-item d-flex justify-content-between align-items-center'><span class='list-item-text'>" .
             $item['id'] . ": " . $item['name'] .
             " " . $item['surname'] . "</span>" .
@@ -15,8 +14,50 @@ class Helper {
             "</a>" .
             "<a href='delete.php?item_id=" . $item['id'] . "'>" .
             "<button class='btn btn-danger'><i class='fa-solid fa-trash-can'></i></button>" .
-            "<a/>" .
+            "</a>" .
             "</div></li>";
+        
+    }
+
+    public static function createContactTable( array $item): string
+    {   
+
+        $id = Self::AccessToValue($item,"id");
+        $name = Self::AccessToValue($item,"name");
+        $surname = Self::AccessToValue($item,"surname");
+        $phone_number = Self::AccessToValue($item,"phone_number");
+        $company = Self::AccessToValue($item,"company");
+        $role = Self::AccessToValue($item,"role");
+        $picture = Self::AccessToValue($item,"picture");
+        $email = Self::AccessToValue($item,"email");
+        $birthdate = Self::AccessToValue($item,"birthdate");
+        $createdAt = Self::AccessToValue($item,"created_at");
+        $active = Self::AccessToValue($item,"active");
+        $pictureId = Self::AccessToValue($item,"picture_id");
+        
+        
+        return  "<tr>" . 
+                    "<th scope='row'>$id</th>" . 
+                    "<td>$name</td>" . 
+                    "<td>$surname</td>" . 
+                    "<td>$phone_number</td>" . 
+                    "<td>$company</td>" . 
+                    "<td>$role</td>" . 
+                    "<td>$picture</td>" . 
+                    "<td>$email</td>" . 
+                    "<td>$birthdate</td>" . 
+                    "<td>$createdAt</td>" . 
+                    "<td>$active</td>" . 
+                    "<td>$pictureId</td>" . 
+                    "<td>" . 
+                        "<div class='list-item-btn-container'>" .
+                            "<a href='update.php?item_id=$id'>" .
+                                "<button class='btn btn-info'><i class='fa-solid fa-pen-to-square'></i></button>" .
+                            "</a>" .
+                            "<button type='button' class='btn btn-danger delete-btn' data-bs-toggle='modal' data-bs-target='#deleteItem'><i class='fa-solid fa-trash-can'></i></button>" .
+                    "</div>" . 
+                    "</td>" . 
+                "</tr>";
     
     }
 
