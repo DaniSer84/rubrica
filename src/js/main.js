d78ddb9cee59c18ac9ac00ac668aa07909072bd3
DaniSer84 <serenelli84@gmail.com>
2024-10-22 23:56:48 +0200

    code refact

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,18 @@
 import { Helper } from "./utilities/Helper.js";
 
-let deleteButton = document.querySelectorAll('.delete-btn')
+let deleteButton = document.querySelectorAll('.delete-btn');
 
 deleteButton.forEach(button => {
     button.addEventListener('click', (e) => {
         
 
         let name = e.target.closest('tr').childNodes[1].textContent + 
-                   " " + e.target.closest('tr').childNodes[2].textContent
+                   " " + e.target.closest('tr').childNodes[2].textContent;
 
         let id = e.target.closest('tr').firstChild.textContent;
 
-        Helper.setToDelete(name, id)
+        Helper.setToDelete(name, id);
 
     })
 });
 
-console.log('riprova')
---
+++ src/js/utilities/Helper.js
@@ -0,0 +1,17 @@
+
+class Helper {
+
+    static setToDelete(name, id) {
+
+        let deleteButton = document.getElementById('delete-btn');
+        let toDelete = document.getElementById('to-delete');
+    
+        toDelete.textContent = name;
+    
+        deleteButton.setAttribute("href", `delete.php?item_id=${id}`);
+    
+    }
+    
+}
+
+export { Helper }
