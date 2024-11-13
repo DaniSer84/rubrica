f0b4614e24dc500f41265c8d17e23f0a4e33aa4a
DaniSer84 <serenelli84@gmail.com>
2024-11-13 12:36:09 +0100

    final commit

--- src/php/components/Head.php
+++ src/php/components/Head.php
@@ -27,16 +27,20 @@ class Head {
   }
   public function render(): string {
 
+    $title = $this->putParam("title");
+    $style = $this->putParam("style");
+    $script = $this->putParam("script");
+
     return
             "<head>
               <meta charset='UTF-8'>
               <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-              <title>" . $this->putParam("title") . "</title>
+              <title>$title</title>
               <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet'
                   integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH' crossorigin='anonymous'>
-              <link rel='stylesheet' href='" . $this->putParam("style") . "'>
+              <link rel='stylesheet' href='$style'>
               <script src='https://kit.fontawesome.com/fb85e57258.js' crossorigin='anonymous'></script>
-              <script src='" . $this->putParam("script") . "' type='module'></script>
+              <script src='$script' type='module'></script>
             </head>";
   }
 }
