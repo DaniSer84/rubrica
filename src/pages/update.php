fbf6610126af757facf394a46b0fe782c361b860
DaniSer84 <serenelli84@gmail.com>
2024-11-19 10:51:46 +0100

    refact

--- src/pages/update.php
+++ src/pages/update.php
@@ -17,6 +17,8 @@ $referer = $_SERVER["HTTP_REFERER"];
 $formRequest = new FormRequest($_REQUEST, $_FILES, $_SERVER, $db);
 
 $data = $formRequest->sendRequest();
+$contact = $data['contact'];
+$picture = $data['picture'];
 
 ?>
 
@@ -41,7 +43,7 @@ $data = $formRequest->sendRequest();
                 <input type="text" name="back-to" value="<?= $referer ?>" hidden>
                 <div class="mb-3 ">
                     <label for="file-upload" class="position-relative text-center">
-                        <img src="<?=$data["picture"][0] !== "" ? $data["picture"][0] : "https://placehold.co/200x200?text=Your+Pic"?>" class="w-50 m-auto add-img-file img-fluid rounded-circle">
+                        <img src="<?=$picture[0] !== "" ? $picture[0] : "https://placehold.co/200x200?text=Your+Pic"?>" class="w-50 m-auto add-img-file img-fluid rounded-circle">
                     </label><br>
                     <span class="img-check d-none"></span>
                     <input type="file" id="file-upload" accept="image/png, image/jpeg" name="picture"
@@ -52,47 +54,47 @@ $data = $formRequest->sendRequest();
                 <div class="form-check form-switch">
                     <input type="hidden" name="active" value="0">
                     <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"
-                        name="active" value="<?= Helper::AccessToValue($data["contact"], 'active') ?>">
+                        name="active" value="<?= Helper::AccessToValue($contact, 'active') ?>">
                     <label class="form-check-label" for="flexSwitchCheckDefault"></label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="id" name="id" placeholder=""
-                        value="<?= Helper::AccessToValue($data["contact"], "id") ?>" readonly>
+                        value="<?= Helper::AccessToValue($contact, "id") ?>" readonly>
                     <label for="id">Id</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="nome" name="name" placeholder="Mario"
-                        value="<?= Helper::AccessToValue($data["contact"], "name") ?>">
+                        value="<?= Helper::AccessToValue($contact, "name") ?>">
                     <label for="nome">Nome</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="cognome" name="surname" placeholder="Rossi"
-                        value="<?= Helper::AccessToValue($data["contact"], "surname") ?>">
+                        value="<?= Helper::AccessToValue($contact, "surname") ?>">
                     <label for="cognome">Cognome</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="tel" class="form-control" id="telefono" name="phone_number" placeholder="02 2021010"
-                        value="<?= Helper::AccessToValue($data["contact"], "phone_number") ?>">
+                        value="<?= Helper::AccessToValue($contact, "phone_number") ?>">
                     <label for="telefono">Telefono</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com"
-                        value="<?= Helper::AccessToValue($data["contact"], "email") ?>">
+                        value="<?= Helper::AccessToValue($contact, "email") ?>">
                     <label for="email">Indirizzo email</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="società" name="company" placeholder="CDM"
-                        value="<?= Helper::AccessToValue($data["contact"], "company") ?>">
+                        value="<?= Helper::AccessToValue($contact, "company") ?>">
                     <label for="società">Società</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="qualifica" name="role" placeholder="Developer"
-                        value="<?= Helper::AccessToValue($data["contact"], "role") ?>">
+                        value="<?= Helper::AccessToValue($contact, "role") ?>">
                     <label for="qualifica">Qualifica</label>
                 </div>
                 <div class="form-floating mb-5">
                     <input type="date" class="form-control" id="data_nascita" name="birthdate" placeholder="01/01/1980"
-                        value="<?= Helper::AccessToValue($data["contact"], "birthdate") ?>" required>
+                        value="<?= Helper::AccessToValue($contact, "birthdate") ?>" required>
                     <label for="data_nascita">Data di nascita</label>
                 </div>
             </div>
